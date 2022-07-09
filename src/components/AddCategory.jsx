f6dcbe56fc339533c50b29fb7721282565dbbef7
Daniel Rodriguez <test@gmail.com>
2022-07-09 11:50:14 -0500

    Testing

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
+import PropTypes from 'prop-types';
 
 export const AddCategory = ({ onNewCategory }) => {
-  const [inputValue, setInputValue] = useState('One Punch');
+  const [inputValue, setInputValue] = useState('');
 
   const onInputChange = ({ target }) => {
     setInputValue(target.value);
@@ -9,7 +10,6 @@ export const AddCategory = ({ onNewCategory }) => {
 
   const onSubmit = (event) => {
     event.preventDefault();
-
     if (inputValue.trim().length <= 1) return;
 
     onNewCategory(inputValue.trim());
@@ -17,7 +17,7 @@ export const AddCategory = ({ onNewCategory }) => {
   };
 
   return (
-    <form onSubmit={onSubmit}>
+    <form onSubmit={onSubmit} aria-label="form">
       <input
         type='text'
         placeholder='Buscar gifs'
@@ -26,4 +26,8 @@ export const AddCategory = ({ onNewCategory }) => {
       />
     </form>
   );
+};
+
+AddCategory.propTypes = {
+  onNewCategory: PropTypes.func.isRequired,
 };
